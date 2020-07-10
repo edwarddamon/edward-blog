0072ae53e562c2df77d82857025a285ac80ed050
edwarddamon <5664023+edwarddamon@user.noreply.gitee.com>
2020-07-10 21:38:52 +0800

    测试test

--- src/main/java/com/lhamster/controller/Test.java
+++ src/main/java/com/lhamster/controller/Test.java
@@ -1,7 +1,12 @@
 package com.lhamster.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
+    @RequestMapping("/test")
+    public String test() {
+        return "666";
+    }
 }
